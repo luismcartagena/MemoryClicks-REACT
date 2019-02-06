461f80e7cad5f5910bfc87d6ad316554ac7bc21d
Luis Cartagena <luiscartagena2017@u.northwestern.edu>
2019-02-06 13:35:11 -0600

    initial build

---
+++ build/precache-manifest.b979a771a24c134667fd1c6467d1dbcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c25d6318a21bda1ab5",
+    "url": "/MemoryClicks-REACT/static/js/runtime~main.81c25d63.js"
+  },
+  {
+    "revision": "3923b508cb9724cc9ffa",
+    "url": "/MemoryClicks-REACT/static/js/main.3923b508.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/MemoryClicks-REACT/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3923b508cb9724cc9ffa",
+    "url": "/MemoryClicks-REACT/static/css/main.3912a7b0.chunk.css"
+  },
+  {
+    "revision": "63b2697c3482b3b5d366ed98cd34327c",
+    "url": "/MemoryClicks-REACT/index.html"
+  }
+];
